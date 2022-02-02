592dbf3984752ee02c64044ee843b9ce91a62a3d
GuiGui15 <guilherme.calpena@gmail.com>
2022-02-02 15:48:49 +0100

    vériflogin

--- src/Controller/CALPENAController.php
+++ src/Controller/CALPENAController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class CALPENAController extends AbstractController
 {
@@ -20,10 +21,18 @@ class CALPENAController extends AbstractController
     /**
      * @Route("/calpena/loginconfirm", name="loginconfirm")
      */
-    public function loginconfirm(): Response
-    {
+    public function loginconfirm(request $request): Response
+    {   
+        $Login = $request -> request -> get("Login");
+        $password = $request -> request -> get("Password");
+        if (($Login=="root") && ($password=="toor")){
+            $reponse = "acces autorise";
+             } 
+             else{
+                 $reponse = "erreur";
+             }
         return $this->render('calpena/loginconfirm.html.twig', [
-            'controller_name' => 'CALPENAController',
+            'Message' => $reponse,
         ]);
     }
 }
